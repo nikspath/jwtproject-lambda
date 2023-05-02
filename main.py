8c8e059526c7ad53a723ba380f7616491c8adde8
nikspath <45727113+nikspath@users.noreply.github.com>
2023-05-02 17:41:31 +0530

    comment password hash

--- main.py
+++ main.py
@@ -117,5 +117,5 @@ async def read_users_me(current_user : User = Depends(get_current_active_user)):
     return current_user
 
 
-pwt=get_password_hash("test")
-print(pwt)
+# pwt=get_password_hash("test")
+# print(pwt)
